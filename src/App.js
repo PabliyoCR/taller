1af26359a46b08c0d8da456b3e2afd2ba24b87b9
PabliyoCR <jimenezpablo94@gmail.com>
2023-08-23 21:48:02 -0600

    4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edittt <code>src/App.js</code> and save to reload.
+          xDD <code>src/App.js</code> and save to reload.
         </p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
